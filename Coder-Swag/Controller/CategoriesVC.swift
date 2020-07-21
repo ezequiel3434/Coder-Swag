52f8815e1219e34fb472774975f5a4739066b7a6
Ezequiel <ezequiel3434@gmail.com>
2020-07-21 19:51:19 -0300

    ProductVC

--- Coder-Swag/Controller/CategoriesVC.swift
+++ Coder-Swag/Controller/CategoriesVC.swift
@@ -42,6 +42,18 @@ extension CategoriesVC: UITableViewDataSource {
         }
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let category = DataService.instance.getCategories()[indexPath.row]
+        performSegue(withIdentifier: "ProductsVC", sender: category)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let productsVC = segue.destination as? ProductVC {
+            assert(sender as? Category != nil)
+            productsVC.initProducts(category: sender as! Category)
+        }
+    }
+    
     
 }
 
---
+++ Coder-Swag/Controller/ProductVC.swift
@@ -0,0 +1,49 @@
+//
+//  ProductVC.swift
+//  Coder-Swag
+//
+//  Created by Ezequiel Parada Beltran on 21/07/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import UIKit
+
+class ProductVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+   
+    @IBOutlet weak var productsCollection: UICollectionView!
+    
+
+    private(set) public var products = [Product]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        productsCollection.dataSource = self
+        productsCollection.delegate = self
+
+    }
+    
+    
+    func initProducts(category: Category){
+        products = DataService.instance.getProducts(forCategoryTitle: category.title)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return products.count
+       }
+       
+       func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ProductCell", for: indexPath) as? ProductCell {
+            let product = products[indexPath.row]
+            cell.updateViews(product: product)
+            return cell
+        }
+        
+        return ProductCell()
+        
+       }
+    
+
+  
+
+}
