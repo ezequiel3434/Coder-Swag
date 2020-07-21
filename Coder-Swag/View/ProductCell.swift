a3a9673dd5abe2bda5fef6b8dc878359040668c7
Ezequiel <ezequiel3434@gmail.com>
2020-07-21 16:47:44 -0300

    Product UIVollectionViewCell

---
+++ Coder-Swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  Coder-Swag
+//
+//  Created by Ezequiel Parada Beltran on 21/07/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
